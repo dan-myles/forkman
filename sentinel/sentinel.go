d0139f11ced5e1201bb3700f68eb562f1ba245d1
avvo-na <79137382+danlikestocode@users.noreply.github.com>
2024-08-31 11:53:04 -0700

    refactor(sentinel): better error handling

--- sentinel/sentinel.go
+++ sentinel/sentinel.go
@@ -1,28 +1,41 @@
 package sentinel
 
 import (
+	"fmt"
+
 	"github.com/avvo-na/devil-guard/config"
 	"github.com/bwmarrin/discordgo"
-	"github.com/rs/zerolog/log"
 )
 
 type Sentinel struct {
 	Session *discordgo.Session
 }
 
-func New() *Sentinel {
+func New() (*Sentinel, error) {
 	s, err := discordgo.New("Bot " + config.AppCfg.DiscordBotToken)
 	if err != nil {
-		log.Panic().Err(err).Msg("Failed to create Discord session! Check your token.")
+		return nil, fmt.Errorf("Failed to create Discord session %w", err)
 	}
 
 	return &Sentinel{
 		Session: s,
+	}, nil
+}
+
+func (s *Sentinel) Start() error {
+	err := s.Session.Open()
+	if err != nil {
+		return fmt.Errorf("Failed to open Discord session %w", err)
 	}
+
+	return nil
 }
 
-func (s *Sentinel) Start() {
-}
+func (s *Sentinel) Stop() error {
+	err := s.Session.Close()
+	if err != nil {
+		return fmt.Errorf("Failed to close Discord session %w", err)
+	}
 
-func (s *Sentinel) Stop() {
+	return nil
 }
