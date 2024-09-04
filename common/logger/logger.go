e08e4f27bf6abf9a385331a68c6a852bd5e22439
avvo-na <79137382+danlikestocode@users.noreply.github.com>
2024-09-04 15:52:53 -0700

    feat(logger): colored logs :P

--- common/logger/logger.go
+++ common/logger/logger.go
@@ -10,16 +10,65 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+// Define ANSI 256 color codes as uncapitalized constants
 const (
-	black = iota + 30
-	red
-	green
-	yellow
-	blue
-	magenta
-	cyan
-	white
-	darkGray = 90
+	// Standard Colors (0-15)
+	black        = 30
+	red          = 31
+	green        = 32
+	yellow       = 33
+	blue         = 34
+	magenta      = 35
+	cyan         = 36
+	white        = 37
+	lightRed     = 91
+	lightGreen   = 92
+	lightYellow  = 93
+	lightBlue    = 94
+	lightMagenta = 95
+	lightCyan    = 96
+	lightWhite   = 97
+
+	// Pastel Colors (specific palette)
+	pastelPink      = 213
+	pastelPeach     = 216
+	pastelPurple    = 141
+	pastelLavender  = 165
+	pastelMint      = 121
+	pastelSkyBlue   = 117
+	pastelCoral     = 210
+	pastelOrange    = 208
+	pastelTurquoise = 79
+	pastelLilac     = 171
+	pastelYellow    = 227
+	pastelBlueGray  = 135
+	pastelGreenGray = 149
+	pastelBeige     = 230
+
+	// Color Cube (16-231)
+	colorCubeStart = 16
+	colorCubeEnd   = 231
+
+	// Grayscale (232-255)
+	grayscaleStart = 232
+	grayscaleEnd   = 255
+)
+
+var (
+	levelColors = map[zerolog.Level]int{
+		zerolog.TraceLevel: grayscaleStart + 15,
+		zerolog.DebugLevel: grayscaleStart + 14,
+		zerolog.InfoLevel:  pastelMint,
+		zerolog.WarnLevel:  yellow,
+		zerolog.ErrorLevel: red,
+		zerolog.FatalLevel: red,
+		zerolog.PanicLevel: red,
+	}
+
+	timeColor            = grayscaleStart + 5
+	sourceColor          = grayscaleStart + 10
+	sourceSeparator      = ">"
+	sourceSeparatorColor = pastelSkyBlue
 )
 
 func Init() {
@@ -27,40 +76,57 @@ func Init() {
 	output.NoColor = false
 	output.FormatLevel = func(i interface{}) string {
 		switch i {
+		case "trace":
+			return colorize(fmt.Sprintf("| %-6s|", i), levelColors[zerolog.TraceLevel])
 		case "debug":
-			return colorize(fmt.Sprintf("| %-6s|", i), darkGray)
+			return colorize(fmt.Sprintf("| %-6s|", i), levelColors[zerolog.DebugLevel])
 		case "info":
-			return colorize(fmt.Sprintf("| %-6s|", i), blue)
+			return colorize(fmt.Sprintf("| %-6s|", i), levelColors[zerolog.InfoLevel])
 		case "warn":
-			return colorize(fmt.Sprintf("| %-6s|", i), yellow)
+			return colorize(fmt.Sprintf("| %-6s|", i), levelColors[zerolog.WarnLevel])
 		case "error":
-			return colorize(fmt.Sprintf("| %-6s|", i), red)
+			return colorize(fmt.Sprintf("| %-6s|", i), levelColors[zerolog.ErrorLevel])
+		case "fatal":
+			return colorize(fmt.Sprintf("| %-6s|", i), levelColors[zerolog.FatalLevel])
+		case "panic":
+			return colorize(fmt.Sprintf("| %-6s|", i), levelColors[zerolog.PanicLevel])
+		default:
+			return colorize(fmt.Sprintf("| %-6s|", i), 0)
 		}
-
-		return strings.ToUpper(fmt.Sprintf("| %-6s|", i))
 	}
 	output.FormatMessage = func(i interface{}) string {
-		if i == "debug" {
-			return colorize(fmt.Sprintf("%s", i), darkGray)
-		}
-
 		return fmt.Sprintf("%s", i)
 	}
 	output.FormatCaller = func(i interface{}) string {
-		if i == "debug" {
-			return colorize(fmt.Sprintf("%s >", i), darkGray)
+		// chop off the path
+		s := fmt.Sprintf("%s", i)
+		s = strings.Split(s, "/")[len(strings.Split(s, "/"))-1]
+
+		// Evenly space
+		// but not too much
+		if len(s) < 15 {
+			s = fmt.Sprintf("%-15s", s)
 		}
 
-		return colorize(fmt.Sprintf("%s", i), darkGray)
+		s = colorize(s, sourceColor)
+		separator := colorize(sourceSeparator, sourceSeparatorColor)
+
+		return fmt.Sprintf("%s%s", s, separator)
 	}
 	output.FormatFieldName = func(i interface{}) string {
-		return fmt.Sprintf("\n%s:", i)
+		// put on new line
+		s := fmt.Sprintf("\n%+59s=", i)
+
+		// colorize
+		s = colorize(s, green)
+
+		return s
 	}
 	output.FormatFieldValue = func(i interface{}) string {
-		return strings.ToUpper(fmt.Sprintf("%s", i))
+		return colorize(fmt.Sprintf("%s", i), grayscaleStart+10)
 	}
 	output.FormatTimestamp = func(i interface{}) string {
-		return colorize(fmt.Sprintf("%s", i), darkGray)
+		return colorize(fmt.Sprintf("%s", i), timeColor)
 	}
 
 	zerolog.SetGlobalLevel(zerolog.DebugLevel)
@@ -68,6 +134,11 @@ func Init() {
 	log.Logger = log.With().Caller().Timestamp().Logger()
 }
 
-func colorize(s string, c int) string {
-	return fmt.Sprintf("\x1b[%dm%v\x1b[0m", c, s)
+// colorize formats a string with the given color code.
+func colorize(s string, color int) string {
+	// 38;5 is the escape sequence for 256-color mode
+	if color < 0 || color > 255 {
+		return s // Return the original string if the color code is out of range
+	}
+	return fmt.Sprintf("\033[38;5;%dm%s\033[0m", color, s)
 }
