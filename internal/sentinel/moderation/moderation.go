ec2304b434c4d23a34783a5090ff48e1209e09d4
avvo-na <79137382+danlikestocode@users.noreply.github.com>
2024-09-05 19:04:21 -0700

    feat(mod): test kick command

--- internal/sentinel/moderation/moderation.go
+++ internal/sentinel/moderation/moderation.go
@@ -8,9 +8,35 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-var commands = []*discordgo.ApplicationCommand{}
+var commands = []*discordgo.ApplicationCommand{
+	{
+		Name:        "kick",
+		Description: "kicks a member from the guild",
+		Type:        discordgo.ChatApplicationCommand,
 
-var commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){}
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Type:        discordgo.ApplicationCommandOptionUser,
+				Name:        "member",
+				Description: "to kick",
+				Required:    true,
+			},
+		},
+	},
+}
+
+var commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
+	"kick": kick,
+}
+
+func kick(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: "Kicking user... (NOT IMPL)",
+		},
+	})
+}
 
 type ModerationModule struct{}
 
@@ -19,7 +45,7 @@ func New() *ModerationModule {
 }
 
 func (u *ModerationModule) Name() string {
-	return "utility"
+	return "moderation"
 }
 
 func (u *ModerationModule) Load(s *discordgo.Session) error {
@@ -55,7 +81,7 @@ func (u *ModerationModule) Load(s *discordgo.Session) error {
 
 	log.Debug().
 		Interface("commands", commands).
-		Msg("Registering utility command handlers...")
+		Msg("Registering moderation command handlers...")
 	s.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		handler, ok := commandHandlers[i.ApplicationCommandData().Name]
 		if !ok {
@@ -65,7 +91,7 @@ func (u *ModerationModule) Load(s *discordgo.Session) error {
 		handler(s, i)
 	})
 
-	log.Debug().Msg("Utility module registration complete")
+	log.Debug().Msg("Moderation module registration complete")
 	return nil
 }
 
@@ -81,7 +107,7 @@ func (u *ModerationModule) Enable(s *discordgo.Session) error {
 
 	// Write new config
 	enable := true
-	config.ModuleCfg.Utility.Enabled = &enable
+	config.ModuleCfg.Moderation.Enabled = &enable
 
 	err := config.WriteConfig()
 	if err != nil {
@@ -89,7 +115,7 @@ func (u *ModerationModule) Enable(s *discordgo.Session) error {
 	}
 	log.Debug().
 		Interface("config", config).
-		Msg("Updated utility module config")
+		Msg("Updated moderation module config")
 
 	// Register all commands
 	for _, command := range commands {
@@ -107,7 +133,7 @@ func (u *ModerationModule) Enable(s *discordgo.Session) error {
 
 	log.Debug().
 		Interface("commands", commands).
-		Msg("Registering utility command handlers...")
+		Msg("Registering moderation command handlers...")
 	s.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		handler, ok := commandHandlers[i.ApplicationCommandData().Name]
 		if !ok {
@@ -117,7 +143,7 @@ func (u *ModerationModule) Enable(s *discordgo.Session) error {
 		handler(s, i)
 	})
 
-	log.Debug().Msg("Utility module registration complete")
+	log.Debug().Msg("Moderation module registration complete")
 	return nil
 }
 
@@ -133,41 +159,41 @@ func (u *ModerationModule) Disable(s *discordgo.Session) error {
 
 	// Disable the module
 	disable := false
-	config.ModuleCfg.Utility.Enabled = &disable
+	config.ModuleCfg.Moderation.Enabled = &disable
 
 	// Write new config
 	err := config.WriteConfig()
 	if err != nil {
 		return fmt.Errorf("Failed to write config: %w", err)
 	}
-	log.Debug().Msg("Updated utility module config")
+	log.Debug().Msg("Updated moderation module config")
 
 	// Get all registered commands
 	registeredCommands, err := s.ApplicationCommands(appID, guildID)
 	if err != nil {
-		return fmt.Errorf("Failed to get registered commands for utility module: %w", err)
+		return fmt.Errorf("Failed to get registered commands for moderation module: %w", err)
 	}
 
-	// Filter out utility commands
-	log.Debug().Msg("Filtering utility commands for deletion...")
-	utilCommands := make([]*discordgo.ApplicationCommand, 0)
-	for _, command := range registeredCommands {
-		for _, utilityCommand := range commands {
-			if command.Name == utilityCommand.Name {
-				utilCommands = append(utilCommands, command)
+	// Filter out moderation commands
+	log.Debug().Msg("Filtering moderation commands for deletion...")
+	cmds := make([]*discordgo.ApplicationCommand, 0)
+	for _, globalCmd := range registeredCommands {
+		for _, modCmd := range commands {
+			if globalCmd.Name == modCmd.Name {
+				cmds = append(cmds, globalCmd)
 			}
 		}
 	}
 
-	// Delete utility commands
-	log.Debug().Msgf("Deleting %d utility commands", len(utilCommands))
-	for _, command := range utilCommands {
+	// Delete moderation commands
+	log.Debug().Msgf("Deleting %d moderation commands", len(cmds))
+	for _, command := range cmds {
 		err := s.ApplicationCommandDelete(appID, guildID, command.ID)
 		if err != nil {
 			return err
 		}
 	}
 
-	log.Debug().Msg("Utility module deregistration complete")
+	log.Debug().Msg("Moderation module deregistration complete")
 	return nil
 }
