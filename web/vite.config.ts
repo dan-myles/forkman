7e1e7086c2bdc188c11ef6211c5819030507fa61
avvo-na <79137382+avvo-na@users.noreply.github.com>
2024-09-13 13:38:48 -0700

    feat: dev server redirection

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "../fork_data/static",
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+    },
+  },
+});
