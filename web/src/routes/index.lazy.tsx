c074488cff634b53a488446d1c628978ff6ae44f
avvo-na <79137382+avvo-na@users.noreply.github.com>
2024-09-13 14:14:47 -0700

    fix: formatting frontend

--- web/src/routes/index.lazy.tsx
+++ web/src/routes/index.lazy.tsx
@@ -1,13 +1,13 @@
-import { createLazyFileRoute } from "@tanstack/react-router";
+import { createLazyFileRoute } from "@tanstack/react-router"
 
 export const Route = createLazyFileRoute("/")({
   component: Index,
-});
+})
 
 function Index() {
   return (
     <div className="p-2 text-2xl">
       <h3>Welcome Home!</h3>
     </div>
-  );
+  )
 }
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import path from "path";
+import path from "path"
+import { defineConfig } from "vite"
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,4 +22,4 @@ export default defineConfig({
       },
     },
   },
-});
+})
