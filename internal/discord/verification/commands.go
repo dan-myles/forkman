da25ad53062e52b729adec38cbe73b99eb39da9a
code-wolf-byte <hackerstark37@gmail.com>
2025-02-26 10:50:11 -0700

    Manual verificaion (#50)
    * Save State
    
    * Added manual verifcaion

--- internal/discord/verification/commands.go
+++ internal/discord/verification/commands.go
@@ -1,6 +1,8 @@
 package verification
 
 import (
+	"os"
+
 	"github.com/avvo-na/forkman/common/colors"
 	"github.com/bwmarrin/discordgo"
 )
@@ -14,6 +16,24 @@ var commands = []*discordgo.ApplicationCommand{
 				Type:        discordgo.ApplicationCommandOptionUser,
 				Name:        "user",
 				Description: "to search for",
+				Required:    true,
+			},
+		},
+	},
+	{
+		Name:        "verify",
+		Description: "manually verify a user's email",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Type:        discordgo.ApplicationCommandOptionUser,
+				Name:        "user",
+				Description: "to verify",
+				Required:    true,
+			},
+			{
+				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "email",
+				Description: "to verify",
 				Required:    true,
 			},
 		},
@@ -66,4 +86,60 @@ func (m *Verification) email(s *discordgo.Session, i *discordgo.InteractionCreat
 			Flags: discordgo.MessageFlagsEphemeral,
 		},
 	})
+}
+
+func (m *Verification) verify(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	print("verify started")
+	member := i.ApplicationCommandData().Options[0].UserValue(s)
+	email := i.ApplicationCommandData().Options[1].StringValue()
+	email, err := m.repo.ManualVerification(i.GuildID, member.ID, email)
+
+	status := "❌ Manual Verification Failed"
+	if err == nil {
+		status = "✅ Manual Verification Successful"
+	}
+
+	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Embeds: []*discordgo.MessageEmbed{
+				{
+					Title: "Verification Status",
+					Color: colors.ASUMaroon,
+					Thumbnail: &discordgo.MessageEmbedThumbnail{
+						URL: member.AvatarURL("4096"),
+					},
+					Fields: []*discordgo.MessageEmbedField{
+						{
+							Name:   "Username",
+							Value:  member.Username,
+							Inline: true,
+						},
+						{
+							Name:   "ID",
+							Value:  member.ID,
+							Inline: true,
+						},
+						{
+							Name:  "Status",
+							Value: status,
+						},
+						{
+							Name:  "Email",
+							Value: email,
+						},
+					},
+				},
+			},
+			Flags: discordgo.MessageFlagsEphemeral,
+		},
+	})
+
+	s.GuildMemberRoleRemove(m.guildSnowflake, i.Member.User.ID, os.Getenv("ROLE_TO_REMOVE"))
+	s.GuildMemberRoleAdd(m.guildSnowflake, i.Member.User.ID, os.Getenv("ROLE_TO_ADD"))
+
+	if err != nil {
+		m.log.Error().Err(err).Msg("Failed to manually verify email")
+	}
+
 }
