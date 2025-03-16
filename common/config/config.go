a5f7d895198b69df90e4acf3a119ae5d5d0514f2
code-wolf-byte <hackerstark37@gmail.com>
2025-03-16 06:31:25 -0700

    fix: load .env file conditionally based on environment

--- common/config/config.go
+++ common/config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"log"
+	"os"
 	"time"
 
 	"github.com/caarlos0/env/v11"
@@ -9,20 +11,20 @@ import (
 
 type ForkConfig struct {
 	// Server Settings
-	DiscordAppID        string        `env:"DISCORD_APP_ID,required,notEmpty"`
-	DiscordClientID     string        `env:"DISCORD_CLIENT_ID,required,notEmpty"`
-	DiscordClientSecret string        `env:"DISCORD_CLIENT_SECRET,required,notEmpty"`
-	DiscordBotToken     string        `env:"DISCORD_BOT_TOKEN,required,notEmpty"`
-	DiscordOwnerID      string        `env:"DISCORD_OWNER_ID,required,notEmpty"`
-	ServerPort          int           `env:"SERVER_PORT,required,notEmpty"`
-	ServerTimeoutRead   time.Duration `env:"SERVER_TIMEOUT_READ,required,notEmpty"`
-	ServerTimeoutWrite  time.Duration `env:"SERVER_TIMEOUT_WRITE,required,notEmpty"`
-	ServerTimeoutIdle   time.Duration `env:"SERVER_TIMEOUT_IDLE,required,notEmpty"`
-	ServerAuthSecret    string        `env:"SERVER_AUTH_SECRET,required,notEmpty"`
-	ServerAuthExpiry    time.Duration `env:"SERVER_AUTH_EXPIRY,required,notEmpty"`
-  ServerAuthCallbackURI string        `env:"SERVER_AUTH_CALLBACK_URI,required,notEmpty"`
-	LogLevel            string        `env:"LOG_LEVEL,required,notEmpty"`
-	GoEnv               string        `env:"GO_ENV,required,notEmpty"`
+	DiscordAppID          string        `env:"DISCORD_APP_ID,required,notEmpty"`
+	DiscordClientID       string        `env:"DISCORD_CLIENT_ID,required,notEmpty"`
+	DiscordClientSecret   string        `env:"DISCORD_CLIENT_SECRET,required,notEmpty"`
+	DiscordBotToken       string        `env:"DISCORD_BOT_TOKEN,required,notEmpty"`
+	DiscordOwnerID        string        `env:"DISCORD_OWNER_ID,required,notEmpty"`
+	ServerPort            int           `env:"SERVER_PORT,required,notEmpty"`
+	ServerTimeoutRead     time.Duration `env:"SERVER_TIMEOUT_READ,required,notEmpty"`
+	ServerTimeoutWrite    time.Duration `env:"SERVER_TIMEOUT_WRITE,required,notEmpty"`
+	ServerTimeoutIdle     time.Duration `env:"SERVER_TIMEOUT_IDLE,required,notEmpty"`
+	ServerAuthSecret      string        `env:"SERVER_AUTH_SECRET,required,notEmpty"`
+	ServerAuthExpiry      time.Duration `env:"SERVER_AUTH_EXPIRY,required,notEmpty"`
+	ServerAuthCallbackURI string        `env:"SERVER_AUTH_CALLBACK_URI,required,notEmpty"`
+	LogLevel              string        `env:"LOG_LEVEL,required,notEmpty"`
+	GoEnv                 string        `env:"GO_ENV,required,notEmpty"`
 
 	// AWS
 	AWS_ACCESS_KEY_ID     string `env:"AWS_ACCESS_KEY_ID,required,notEmpty"`
@@ -40,7 +42,12 @@ type ForkConfig struct {
 }
 
 func New() *ForkConfig {
-	godotenv.Load()
+	if os.Getenv("GO_ENV") != "production" {
+		if err := godotenv.Load(); err != nil {
+			log.Println("No .env file found, relying on external env vars...")
+		}
+	}
+
 	cfg, err := env.ParseAs[ForkConfig]()
 	if err != nil {
 		panic(err)
